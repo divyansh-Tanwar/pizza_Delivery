13cc1f5b8503cd14c5763e71c36859615d8a4d59
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-07 19:23:08 +0530

    setting up ejs template to use

--- routes/server.js
+++ routes/server.js
@@ -1,7 +1,14 @@
 const express= require("express");
 const app= express();
 const PORT=process.env.PORT||3000;
+//step1:require ejs 
+const ejs=require('ejs')
+const expressJsLayout=require('express-ejs-layouts')
+// step2:setting template engine
+app.use(expressJsLayout);
 
+
+//handeling requests
 app.get("/",function(req,res){
     res.send("Hello from server");
 })
