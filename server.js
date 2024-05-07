ba7bd56e93f18fbf29b94753962dac4c9f22c112
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-07 19:40:21 +0530

    ejs setup complete

---
+++ server.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const app= express();
+const path=require('path');
+const PORT=process.env.PORT||3000;
+//step1:require ejs 
+const ejs=require('ejs')
+//step2:telling express from where he has to pic ejs files (setting file path )
+app.set('views',path.join(__dirname,'/resources/views'));
+//step3:telling express which template engine to use
+app.set('view engine','ejs');
+
+
+//handeling requests
+app.get("/",function(req,res){
+    res.render('home');
+})
+
+app.listen(PORT,function(){
+    console.log(`listening on port ${PORT}`)
+})
