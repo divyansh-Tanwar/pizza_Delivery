cd0efe2a2468bdb4d18556ff6caeaa031b9416da
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-10 22:33:28 +0530

    navbar for desktop screen

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express= require("express");
 const app= express();
 const path=require('path');
 const PORT=process.env.PORT||3000;
-//step1:require ejs 
+//telling express about static folder fro mwhere it has to pick css/js files
+app.use(express.static('public'));
 const ejs=require('ejs')
-//step2:telling express from where he has to pic ejs files (setting file path )
 app.set('views',path.join(__dirname,'/resources/views'));
-//step3:telling express which template engine to use
 app.set('view engine','ejs');
 
 
