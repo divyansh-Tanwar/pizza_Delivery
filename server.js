e15e4ffd4e622c668d6e47fc52687c75495254b1
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-05-17 20:00:19 +0530

    setting up layout layer to remove redundency of code

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express= require("express");
 const app= express();
 const path=require('path');
+const expressLayout=require("express-ejs-layouts");
 const PORT=process.env.PORT||3000;
 //telling express about static folder fro mwhere it has to pick css/js files
 app.use(express.static('public'));
 const ejs=require('ejs')
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+app.use(expressLayout);
 
 
 //handeling requests
 app.get("/",function(req,res){
     res.render('home');
+})
+
+app.get("/cart",function(req,res){
+    res.render("customers/cart");
 })
 
 
