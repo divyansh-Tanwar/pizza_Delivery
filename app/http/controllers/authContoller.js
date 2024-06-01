e0c07556b04d865077ed3c5fdd6012023379d7b0
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2024-06-01 19:25:01 +0530

    new user successfully saved in database

--- app/http/controllers/authContoller.js
+++ app/http/controllers/authContoller.js
@@ -1,4 +1,6 @@
+const { error } = require("laravel-mix/src/Log");
 const User=require("../../models/User");
+const bcrypt=require("bcrypt");
 function authController(){
     return {
 
@@ -12,9 +14,10 @@ function authController(){
             res.render("Auth/register");
         },
 
-        postRegister(req,res)
+        async postRegister(req,res)
         {
             const {name,Username,password}=req.body;
+              console.log(req.body);
             //data validation
             if(!name||!Username||!password)
             {   
@@ -24,7 +27,41 @@ function authController(){
 
                 return res.redirect("/register");
             }
-            console.log(req.body);
+             
+
+            //checking if email already exist in database or not 
+              if(await User.exists({Email:Username}))
+              {      
+                    console.log("user exists");
+                    req.flash('error','User Already Exists')
+                    req.flash('name',name)
+                    req.flash('Username',Username)
+                    return res.redirect("/register");
+              }
+              //hashing the passowrd
+              const pass= await bcrypt.hash(password,10); 
+
+              //creating new user
+              const newUser= new User({
+                name:name,
+                Email:Username,
+                password:pass
+              });
+
+              //if user is created successfully
+              newUser.save().then((user)=>{
+                 console.log('user saved successfully');
+                return res.redirect("/login");
+
+              }).catch(error=>{
+                req.flash('error','Please Try again')//for the notification 
+                req.flash('name',name)
+                req.flash('Username',Username)
+                return res.redirect("/register");
+              }); 
+
+              
+          
         }
 
     }
